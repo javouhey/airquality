0785cfd9866eb1a038ff737413e06a7cff2f6cdb
Gavin Bong <rubycoder@gmail.com>
2014-02-09 13:24:01 -0800

    Fixed logfile

--- airquality/settings.py
+++ airquality/settings.py
@@ -5,4 +5,4 @@ STORAGE_ENGINE = ('mongo', 'airquality.backends.mongo.MongoRepository')
 MONGO_COLLECTION = 'readings'
 MONGO_DATABASE = 'test'
 MONGO_URL = 'mongodb://localhost:44444/'
-LOG_FILE = '/tmp/aqi/airquality.log'
+LOG_FILE = '/tmp/airquality.log'
